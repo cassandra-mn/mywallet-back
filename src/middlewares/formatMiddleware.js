8b4ffbadb140d515e71e1f858eb2f603d2cb3d67
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-05-19 09:32:33 -0300

    Preparando revisão de código

--- src/middlewares/formatMiddleware.js
+++
@@ -1,8 +0,0 @@
-import transactionSchema from '../schemas/transactionSchema.js';
-
-export function validateFormat(req, res, next) {
-    const {error} = transactionSchema.validate(req.body, {abortEarly: false});
-    if (error) return res.status(422).send(error.details.map(detail => detail.message));
-
-    next();
-}
--- src/schemas/transactionSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-const transactionSchema = joi.object({
-    value: joi.number().required(),
-    description: joi.string().required()
-});
-
-export default transactionSchema;
